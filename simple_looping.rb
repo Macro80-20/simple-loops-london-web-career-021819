67c6de752bd78282a3a7ff9e934cf21a01214332
fernando <fernando_lopezc@live.com>
2019-02-06 08:21:08 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,6 +8,7 @@ phrase = "Welcome to Flatiron School's Web Development Course!"
     counter+=
     break if counter == 7
   end
+  loop_iterator
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer
   # The integer is the number of times the loops should puts out the phrase
